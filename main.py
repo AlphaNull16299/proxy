82f748bf04e6f11dbd5746be494f98c669da0c30
AlphaNull <58626192+AlphaNull16299@users.noreply.github.com>
2021-07-25 17:01:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,20 @@
 
 global proxytype
 proxytype = {}
-from tqdm import tqdm
-import os, requests, socket, socks, time, random, threading, sys, ssl, datetime, cfscrape, re
-from time import sleep
-global out_file
-out_file = "proxy.txt"
+try:
+    from tqdm import tqdm
+    import os, requests, socket, socks, time, random, threading, sys, datetime, re
+except:
+    print("必要なモジュールがインストールされていません。\nなので今から勝手にインストールします")
+    os.system("python3 -m pip install requests pysocks tqdm")
+    os.system("python -m pip install requests pysocks tqdm")
+    try:
+        from tqdm import tqdm
+        import os, requests, socket, socks, time, random, threading, sys, datetime, re
+    except:
+        print("はい、もうこの自動化されたスクリプトがやれるのはここまでです。\nもう無理なので手動でやってください！\nコマンド: pip3 install requests pysocks tqdm")
+
+max_thread = int(input("最大スレッド数どうする?(おすすめは500、制限なくすには0): "))
 
 def checking(lines, socks_type, ms):
     global proxytype
@@ -53,92 +62,80 @@ nums = 0
 def check_socks(ms):
     global nums
     thread_list = []
-    print("スレッドを開始中...")
+    print("スレッドを開始中なう...")
     for lines in tqdm(list(proxies)):
         th = threading.Thread(target=checking, args=(lines, 1, ms,))
         th.start()
         thread_list.append(th)
         time.sleep(0.0001)
-#        while len(threading.enumerate()) >= max_thread+1:time.sleep(1)
-    print("完了\nプロキシの確認を完了中...")
+        while (len(threading.enumerate()) >= max_thread+1) and max_thread != 0:time.sleep(1)
+    print("完了\nプロキシの確認を完了中なう...")
     for th in tqdm(list(thread_list)):
         try:th.join()
         except:pass
     print("完了")
-    print("書き込み中...",end='')
-    ans = "y"
-    if ans == "y" or ans == "":
-        if "4" == "4":
-            try:os.mkdir("check-results")
-            except:pass
-            with open("check-results/socks4.txt", 'wb') as fp:
-                for lines in list(proxies):
-                    if proxytype[lines.replace("\n","")] == 4:
-                        fp.write(bytes(lines, encoding='utf8'))
-            fp.close()
-            with open("check-results/socks5.txt", 'wb') as fp:
-                for lines in list(proxies):
-                    if proxytype[lines.replace("\n","")] == 5:
-                        fp.write(bytes(lines, encoding='utf8'))
-            fp.close()
-            with open("check-results/http.txt", 'wb') as fp:
-                for lines in list(proxies):
-                    if proxytype[lines.replace("\n","")] == 1:
-                        fp.write(bytes(lines, encoding='utf8'))
-            fp.close()
-            with open("check-results/mix.txt", 'wb') as fp:
-                for lines in list(proxies):
-                    if proxytype[lines.replace("\n","")] == 1:out_type = "http"
-                    if proxytype[lines.replace("\n","")] == 4:out_type = "socks4"
-                    if proxytype[lines.replace("\n","")] == 5:out_type = "socks5"
-                    fp.write(bytes(out_type+"://"+lines.replace("\n","")+"/\n", encoding='utf8'))
-            fp.close()
+    print("書き込み中なう...",end='')
+    try:os.mkdir("check-results")
+    except:pass
+    with open("check-results/socks4.txt", 'wb') as fp:
+        for lines in list(proxies):
+            if proxytype[lines.replace("\n","")] == 4:
+                fp.write(bytes(lines, encoding='utf8'))
+    fp.close()
+    with open("check-results/socks5.txt", 'wb') as fp:
+        for lines in list(proxies):
+            if proxytype[lines.replace("\n","")] == 5:
+                fp.write(bytes(lines, encoding='utf8'))
+    fp.close()
+    with open("check-results/http.txt", 'wb') as fp:
+        for lines in list(proxies):
+            if proxytype[lines.replace("\n","")] == 1:
+                fp.write(bytes(lines, encoding='utf8'))
+    fp.close()
+    with open("check-results/mix.txt", 'wb') as fp:
+        for lines in list(proxies):
+            if proxytype[lines.replace("\n","")] == 1:out_type = "http"
+            if proxytype[lines.replace("\n","")] == 4:out_type = "socks4"
+            if proxytype[lines.replace("\n","")] == 5:out_type = "socks5"
+            fp.write(bytes(out_type+"://"+lines.replace("\n","")+"/\n", encoding='utf8'))
+    fp.close()
     print("完了\ncheck-resultsフォルダー内の、http.txt socks4.txt socks5.txtに保存しました。")
 
 
 def check_list(socks_file):
     temp = open(socks_file).readlines()
     temp_list = []
-    print("処理中(1/2)")
+    print("処理中なう(1/2)")
     for i in tqdm(temp):
         if i not in temp_list:
             if ':' in i:
                 temp_list.append(i)
     rfile = open(socks_file, "wb")
-    print("処理中(2/2)")
+    print("処理中なう(2/2)")
     for i in tqdm(list(temp_list)):
         rfile.write(bytes(i, encoding='utf-8'))
     rfile.close()
 
 
-def downloadsocks(choice):
-    global out_file
-    if "4" == "4":
-        f = open(out_file, 'wb')
-        try:
-            r = requests.get("https://midokuriserver.com/proxytool/",timeout=500)
-            f.write(r.content)
-        except:
-            pass
+def downloadsocks():
+    f = open("proxy.txt", 'wb')
+    try:
+        r = requests.get("https://midokuriserver.com/proxytool/",timeout=500)
+        f.write(r.content)
+    except:
+        pass
 
 
-def proxydl(out_file, socks_type):
+def proxydl():
     global proxies, multiple, choice, data
-    ms = 5
-    if socks_type == 1:
-        socktyper = "HTTP"
-    if socks_type == 4:
-        socktyper = "SOCKS4"
-    if socks_type == 5:
-        socktyper = "SOCKS5"
-
-    print("プロキシをダウンロードしています...")
+    ms = int(input("タイムアウトの時間どうする?(おすすめは5秒): "))
+    print("プロキシをダウンロードなう...")
     start = time.time()
-    downloadsocks(1)
+    downloadsocks()
     end = time.time() - start
     print(" 完了("+str(end)+"秒)")
-    proxies = open(str(out_file)).readlines()
-    check_list(out_file)
+    proxies = open(str("proxy.txt")).readlines()
+    check_list("proxy.txt")
     check_socks(ms)
 
-proxydl(out_file,1)
+proxydl()
